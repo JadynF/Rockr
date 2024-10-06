f4308b53085df0de7fb4c84438b6fda2c6424f8a
JadynF <jrf038@email.latech.edu>
2024-10-06 13:59:46 -0500

    Added merged frontend projects

---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+function Header({ isMenuVisible, toggleMenu } ) {
+    return (
+        <div className = 'header'>
+            <h2 onClick = {toggleMenu}> {isMenuVisible ? 'Hide Menu' : 'Show Menu'} </h2>
+            <h1> Rockr </h1>
+        </div>
+    );
+}
+
+export default Header;
