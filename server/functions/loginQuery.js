0ec8556b6cd4c23f32ab88ad07b42011bb1f9a12
Jacob Stein <steinjbusiness@gmail.com>
2024-10-07 16:31:33 -0500

    Created template functions for sql query creation. Server was updated with comments for future connection and query to sql database and return statements to frontend after queries.

---
+++ server/functions/loginQuery.js
@@ -0,0 +1,6 @@
+export default function loginQuery(object){
+    let uNameInput = object.username;
+
+    queryStr = "SELECT password FROM <table_name> WHERE username=" + uNameInput;
+    return queryStr;
+}
--- server/server.js
+++ server/server.js
@@ -1,3 +1,6 @@
+import loginQuery from './functions/loginQuery';
+import createAcctQuery from './functions/createAcctQuery';
+
 const express = require('express');
 const cors = require('cors');
 
@@ -5,6 +8,22 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+/*
+//Establishes credentials to connect to MySQL database 
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'progDuck123*',
+  database: 'screamin_oaks_database'
+});
+//Attempts connection to MySQL database
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected!');
+});
+*/
 
 app.post('/login', (req, res) => {
   let body = req.body;
@@ -14,18 +33,33 @@ app.post('/login', (req, res) => {
   else{
     return res.send(JSON.stringify({response: "rejected"}));
   }
+  /*
+    const body = req.body
+    let loginQueryStr = loginQuery(body);
+    connection.query(loginQueryStr, (error, results, fields) => {
+      if (error) return res.send(error);
+      if(results == body.password){
+        return res.send(JSON.stringify({response: "accepted"}));
+      } else {
+        return res.send(JSON.stringify({response: "rejected"}));
+      }
+    });
+  */
 });
 
 app.post('/AcctCreation', (req, res) => {
+    return res.send(JSON.stringify(req.body));
     /*
     const body = req.body
-    loginQuery = magicalMakeSQLQuery(body);
-    connection.query(body.string, (error, results, fields) => {
-      if (error) return res.send(error);
-      return res.send(JSON.stringify(results));
+    let createAcctStr = createAcctQuery(body);
+    connection.query(createAcctStr, (error, results, fields) => {
+      if (error){
+        return res.send(JSON.stringify({reponse: "Unexpected error."}));
+      } else {
+        return res.send(JSON.stringify({response: "accepted"}));
+      }
     });
     */
-    return res.send(JSON.stringify(req.body));
 });
 
 app.listen(8000, () => {
