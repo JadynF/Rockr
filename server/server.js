fa9df360c8964b1f0ff663ab9d1fdc3039e8741f
Jacob Stein <steinjbusiness@gmail.com>
2024-10-15 17:34:42 -0500

    Fixing node_modules errors

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 
 const loginQuery = require("./functions/loginQuery.js");
